d2d93861368abc05aa0839f051d5ea2dc3120d1c
Parthasai <53685926+propardhu@users.noreply.github.com>
2022-01-23 12:26:57 +0530

    adding

---
+++ src/main/java/com/pardhu/demoWebsocket/DemoWebsocketApplication.java
@@ -0,0 +1,13 @@
+package com.pardhu.demoWebsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoWebsocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoWebsocketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pardhu/demoWebsocket/DemoWebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pardhu.demoWebsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoWebsocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
