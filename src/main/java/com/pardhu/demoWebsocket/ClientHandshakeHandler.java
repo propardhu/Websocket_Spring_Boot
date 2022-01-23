ddd550c15955305aad7c098c7be0f4f8404c7bfb
Parthasai <53685926+propardhu@users.noreply.github.com>
2022-01-23 12:53:39 +0530

    WebSocket Config and unique id per session

---
+++ src/main/java/com/pardhu/demoWebsocket/ClientHandshakeHandler.java
@@ -0,0 +1,24 @@
+package com.pardhu.demoWebsocket;
+
+import com.sun.security.auth.UserPrincipal;
+import java.security.Principal;
+import java.util.Map;
+import java.util.UUID;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
+
+public class ClientHandshakeHandler extends DefaultHandshakeHandler {
+
+  private final Logger logger = LoggerFactory.getLogger(ClientHandshakeHandler.class);
+
+  @Override
+  protected Principal determineUser(ServerHttpRequest req, WebSocketHandler weHandler, Map<String, Object> attributes) {
+    final String randId = UUID.randomUUID().toString();
+    logger.info("User opened client unique ID {}, ipAddress {}",randId,req.getRemoteAddress());
+    return new UserPrincipal(randId);
+  }
+
+}
---
+++ src/main/java/com/pardhu/demoWebsocket/WSConfig.java
@@ -0,0 +1,25 @@
+package com.pardhu.demoWebsocket;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WSConfig implements WebSocketMessageBrokerConfigurer {
+
+  @Override
+  public void configureMessageBroker(final MessageBrokerRegistry registry){
+    registry.enableSimpleBroker("/do");
+    registry.setApplicationDestinationPrefixes("/ws");
+  }
+
+  @Override
+  public void registerStompEndpoints(final StompEndpointRegistry registry) {
+    registry.addEndpoint("/websocket")
+        .setHandshakeHandler(new ClientHandshakeHandler())
+        .withSockJS();
+  }
+}
